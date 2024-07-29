819d7a79379a936371279baec26cd404ba8feffb
bahylol <bahymohamed2010@gmail.com>
2024-07-29 22:36:04 +0300

    filter done

--- Backend/controller/filtter.js
+++ Backend/controller/filtter.js
@@ -1,10 +1,40 @@
-// const { isEmpty } = require('lodash');
+const { productModel } = require("../model/products");
 
 module.exports = function (app) {
-    app.get('/filter', async function (req, res) {
-        return res.status(200).send('Test');
 
+    app.get('/products', async function (req, res) {
+        const category = req.query.category;
+        const minPrice = parseFloat(req.query.minPrice);
+        const maxPrice = parseFloat(req.query.maxPrice);
+        const brand = req.query.brand;
+
+        // Construct the query object
+        let query = {};
+
+        if (category) {
+            query.category = category;
+        }
+        if (!isNaN(minPrice)) {
+            query.price = query.price || {};
+            query.price.$gte = minPrice;
+        }
+        if (!isNaN(maxPrice)) {
+            query.price = query.price || {};
+            query.price.$lte = maxPrice;
+        }
+        if (brand) {
+            query.brand = brand;
+        }
+
+        try {
+            const products = await productModel.find(query);
+            return res.status(200).json(products);
+        } catch (err) {
+            return res.status(500).send('Error filtering products');
+        }
     });
+
+
 }
 
 
