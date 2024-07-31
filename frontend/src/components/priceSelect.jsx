94f2a59b03ff3300bb38939891181f19fffa6766
bahylol <bahymohamed2010@gmail.com>
2024-07-31 23:31:24 +0300

    Create priceSelect.jsx

---
+++ frontend/src/components/priceSelect.jsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Slider from '@mui/material/Slider';
+
+function valuetext(value) {
+  return `$${value}`;
+}
+
+const PriceSelect = ({ minPrice, maxPrice, onChange }) => {
+  const [value, setValue] = React.useState([minPrice, maxPrice]);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+    onChange(newValue);
+  };
+
+  return (
+    <Box sx={{ width: 300 }}>
+      <Slider
+        getAriaLabel={() => 'Price range'}
+        value={value}
+        onChange={handleChange}
+        valueLabelDisplay="auto"
+        getAriaValueText={valuetext}
+        min={0}
+        max={1000} 
+      />
+    </Box>
+  );
+};
+
+export default PriceSelect;
